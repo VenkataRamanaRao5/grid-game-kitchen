204cbacafffe843d659c7635d01e289c38265884
C. V. Venkata Ramana Rao <venkatraocv2005@gmail.com>
2025-03-16 15:32:18 +0530

    working button

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
 enablePlugins(ScalaJSPlugin)
 
-name := "Scala.js Tutorial"
+name := "Grid Game Kitchen"
 scalaVersion := "3.5.2" // or a newer version such as "3.4.2", if you like
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "2.8.0"
--- src/main/scala/gridgamekitchen/HelloWorld.scala
+++ src/main/scala/gridgamekitchen/HelloWorld.scala
@@ -1,7 +1,22 @@
 package gridgamekitchen
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+def appendPar(targetNode: dom.Node, text: String): Unit = {
+  val parNode = document.createElement("p")
+  parNode.textContent = text
+  targetNode.appendChild(parNode)
+}
+
+@JSExportTopLevel("addClickedMessage")
+def addClickedMessage(): Unit = {
+  appendPar(document.body, "You clicked the button!")
+}
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello, world!")
   }
 }
